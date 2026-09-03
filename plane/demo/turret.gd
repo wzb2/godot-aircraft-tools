91cf49c2a0da0fbeefaa16f9e999ae5d31aa0cba
wesley <wzbin2@gmail.com>
2026-09-02 20:47:26 -0700

    misc stuff

--- plane/demo/turret.gd
+++ plane/demo/turret.gd
@@ -1,21 +1,28 @@
-extends StaticBody3D
+extends Node3D
 
 class_name Turret
 
 @export var gun: MachineGun
 
+@export var vehicle: RigidBody3D
+
 @export var target: Node3D
 
 @export var max_range: float = 600
 
+@export var drop_fudge: float = 3
+@export var tof_fudge: float = 1
+
+
 func _physics_process(_delta: float) -> void:
 	var dist_to_target: float = global_position.distance_to(target.global_position)
 	var tof: float = dist_to_target / gun.muzzle_speed
-	var drop: Vector3 = Vector3.DOWN * 9.8 * tof**2.0 * 0.5 * 3
+	var drop: Vector3 = Vector3.DOWN * 9.8 * tof**2.0 * 0.5 * drop_fudge
+	var vel: Vector3 = vehicle.linear_velocity if vehicle else Vector3.ZERO
 	if target is RigidBody3D:
-		gun.look_at(target.global_position + target.linear_velocity * tof * 1.25 - drop)
+		gun.look_at(target.global_position + (target.linear_velocity - vel) * tof * tof_fudge - drop)
 	else:
-		gun.look_at(target.global_position - drop)
+		gun.look_at(target.global_position - vel * tof - drop)
 	
 	gun.firing = dist_to_target < max_range
 	
